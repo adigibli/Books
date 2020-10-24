b4104952e19f6dbe84fa82555c726534bacb7d46
adigibli <gibliadi@gmail.com>
2020-10-24 21:01:06 +0300

    Add project files.

---
+++ Data/BooksContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Books.Data
+{
+    public class BooksContext : DbContext
+    {
+        public BooksContext (DbContextOptions<BooksContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Books.Data.Book> Book { get; set; }
+    }
+}
---
+++ Migrations/20201024171306_AddImagePath.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Books.Migrations
+{
+    public partial class AddImagePath : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImagePath",
+                table: "Book",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImagePath",
+                table: "Book");
+        }
+    }
+}
